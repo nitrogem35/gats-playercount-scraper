70992c983871c36e650e2177c61d75844b51255b
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2022-10-15 21:31:55 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const XMLHttpRequest = require("xmlhttprequest-ssl")
+const express = require('express')
+const app = express()
+/**
+ * @param {int seconds} pollInterval
+ * @param {int minutes} trackingDuration
+*/
+var pollInterval = 30
+var trackingDuration = 5
+
+var history = []
+
+app.get('/', (req, res) => {
+    res.status(200).send(JSON.stringify(history))
+})
+
+app.listen(8000)
+
+var fetchPlayerCountLoop = setInterval(() => {
+    fetchPlayerCount()
+}, pollInterval * 1000)
+
+fetchPlayerCount()
+
+function fetchPlayerCount() {
+    var playerCount = 0
+    var request = new XMLHttpRequest()
+    request.open('POST', 'https://io-8.com/find_instances', true)
+    request.setRequestHeader('Content-Type', 'application/json')
+    var info = {
+        game: 'gats.io',
+    }
+    request.send(JSON.stringify(info))
+    request.onload = function () {
+        var resp = JSON.parse(this.responseText)
+        for (var i = 0; i < resp.length; i++) {
+            playerCount += resp[i].players
+        }
+        history.push({ time: Date.now(), count: playerCount })
+        history = history.slice(-Math.floor(((60 / pollInterval) * trackingDuration)))
+    }
+}
